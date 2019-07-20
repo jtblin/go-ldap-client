75f7f3d64e90f9eb17b0d70bf68fcf3eea0c28f2
simonjanss <38363606+simonjanss@users.noreply.github.com>
2019-07-20 13:37:03 +0200

    Added example for AuthenticateGroups

--- example_test.go
+++ example_test.go
@@ -47,3 +47,39 @@ func ExampleLDAPClient_GetGroupsOfUser() {
 	}
 	log.Printf("Groups: %+v", groups)
 }
+
+// ExampleLDAPClient_AuthenticateGroups shows how to check if user is in the allowed groups
+func ExampleLDAPClient_AuthenticateGroups() {
+	client := &ldap.LDAPClient{
+		Base:         		 "dc=example,dc=com",
+		Host:         		 "ldap.example.com",
+		Port:         		 389,
+		UseSSL:       		 false,
+		BindDN:       		 "uid=readonlyuser,ou=People,dc=example,dc=com",
+		BindPassword: 		 "readonlypassword",
+		UserFilter:   		 "(sAMAccountName=%s)",
+		GroupsAllowed: 		 []string{"Testgroup", "Webgroup", "Admingroup"},
+		GroupsAllowedFilter: "(&(sAMAccountName=%s)(memberOf=CN=%s,dc=example,dc=com))",
+		Attributes:   		 []string{"cn", "givenName", "sn", "mail", "uid"},
+	}
+
+	defer client.Close()
+	
+	ok, user, err := client.Authenticate("username", "password")
+	if err != nil {
+		log.Fatalf("Error authenticating user %s: %+v", "username", err)
+	}
+	if !ok {
+		log.Fatalf("Authenticating failed for user %s", "username")
+	}
+	log.Printf("User: %+v", user)
+	
+	groups, err := client.AuthenticateGroups("username")
+	if err != nil {
+	log.Printf("Error getting groups for user %s: %+v", "username", err)
+	} else if groups == false {
+	log.Printf("Authentication failed: User %s is not in the allowed groups.", "username")
+	} else {
+	log.Printf("Authentication successful: User %s is logged in.", "username")
+	}
+}
